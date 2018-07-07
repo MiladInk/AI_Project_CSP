7ab95af54cc2833b5d297d252e437b88a90a9887
miladink <mi.aghajohari@gmail.com>
2018-07-08 01:45:42 +0430

    dpll completed + git ignore added

--- dpll.cpp
+++ dpll.cpp
@@ -5,23 +5,26 @@
 #include<random>
 #include<algorithm>
 #include<vector>
-#include<map>
+#include<unordered_map>
 #include<unordered_set>
-
+#define musi unordered_map<int, unordered_set<int>>
 #define MAX_DIM 500
 #define MAX_PIECE 50
-clock_t unitc, cnfc, purec, choosec;
+clock_t unitc, cnfc, purec, choosec, crd;
+int dpllcall =0, dplldepth = 0;
 
-#define print(x) cout << x << endl
+#define print(x) cerr << x 
 using namespace std;
 bool var_array[MAX_PIECE][MAX_DIM][MAX_DIM][4];
-int m, n, p, field[MAX_DIM][MAX_DIM];
-map<int, unordered_set<int>> var_rule;
+int m, n, p, field[MAX_DIM][MAX_DIM], return_depth = -1;
+musi var_rule, rule_var, gvar_rule, grule_var;
 unordered_set<int> active_rules;
 vector<int> deactivated_rules;
 vector<pair<int, int>> removed_variables;
 unordered_set<int> unit_rules;
-map<int, unordered_set<int>> rule_var;
+unordered_set<int> pure_candidates;
+unordered_map<int,int> decl;
+
 bool exit_null_rule = false;
 
 bool get(int index) {
@@ -41,6 +44,24 @@ bool get(int index) {
     if (negate)
         value = !value;
     return value;
+}
+
+void print_human(int index){
+    bool negate = false;
+    if(index < 0){
+        negate = true;
+        index = -index;
+    }
+    int k = index/(m*n*4);
+    index %= m*n*4;
+    int m = index/(n*4);
+    index %= n*4;
+    int n = index/4;
+    int d = (index%4);
+    if(negate)
+        cout <<"not";
+    cerr << "("<<k<<","<<m<<","<<n<<","<<d<<")";
+    
 }
 
 
@@ -106,7 +127,39 @@ bool collision(int x, int y, int piece_index) {
     return false;
 }
 
+int calc_return_depth(int index){
+    int res = -1;
+    // if(dpllcall%500<10)
+    //     cout << "you spend"<< (double)crd/CLOCKS_PER_SEC << "in crd" << endl;
+    clock_t begin = clock();
+    for(auto& rule: gvar_rule[index]){
+        for(auto var: grule_var[rule]){
+            if(var_rule[var].empty() && var_rule[-var].empty() && decl[var]>res)
+                res = decl[var];
+                if(res == dplldepth -1)
+                    return res;
+        }
+    }
+    for(auto& rule: gvar_rule[-index]){
+        for(auto var: grule_var[rule]){
+            if(var_rule[var].empty() && var_rule[-var].empty() && decl[var]>res)
+                res = decl[var];
+            if(res == dplldepth -1)
+                    return res;
+        }
+    }
+    crd += clock()-begin;
+    //  if(res!= -1)
+    //      cout <<"return depth is" << res << "but "<< dplldepth << endl;
+    return res;
+}
+
 void cnf(int index, bool value) {
+    clock_t begin = clock();
+    // if((dpllcall%500)==0)
+    //     cout << "time in cnf is " <<(double)cnfc/CLOCKS_PER_SEC << endl;
+    decl[index] = dplldepth;
+    decl[-index] = dplldepth; 
     if (value == false)
         index = -index;
     set(index, true);
@@ -115,6 +168,8 @@ void cnf(int index, bool value) {
             if (index == index2)
                 continue;
             var_rule[index2].erase(rule);
+            if(var_rule[-index2].empty())
+                pure_candidates.insert(index2);
             removed_variables.push_back(make_pair(index2, rule));
         }
         removed_variables.push_back(make_pair(index, rule));
@@ -133,18 +188,19 @@ void cnf(int index, bool value) {
         }
     }
     var_rule[-index].clear();
+    cnfc+= clock()-begin;
 }
 
 bool pure() {
     clock_t begin = clock();
     bool changed = false;
-    for (auto &kv: var_rule) {
-        if (var_rule[-kv.first].empty() && !kv.second.empty()) {
-            cnf(kv.first, true);
+    for (auto can: pure_candidates) {
+        if (var_rule[-can].empty() && !var_rule[can].empty()) {
+            cnf(can, true);
             changed = true;
-            break;
         }
     }
+    pure_candidates.clear();
     clock_t end = clock();
     purec = end - begin;
     return changed;
@@ -226,18 +282,16 @@ void print_color_map() {
     }
 }
 
-bool find_empty_rule(vector<vector<int>> rules) {
-    for (auto &rule: rules) {
-        if (rule.empty())
-            return true;
-    }
-    return false;
-}
-
 int choose_index() {
-    int rule = *active_rules.begin();
-
-    return *rule_var[rule].begin();
+    int key = 0;
+    int value = 0;
+    for(auto& kv:var_rule){
+        if(kv.first >0 && kv.second.size()+var_rule[-kv.first].size()>value){
+            value = kv.second.size();
+            key = kv.first;
+        }
+    }
+    return key;
 }
 
 void revert(vector<int> deactivated_rules_local, vector<pair<int, int>> removed_variables_local) {
@@ -250,9 +304,27 @@ void revert(vector<int> deactivated_rules_local, vector<pair<int, int>> removed_
     }
 }
 
+
+
+bool goahead(){
+    if(return_depth == -1 || dplldepth == return_depth){
+        return_depth = -1;
+        return true;
+    }
+    return false;
+}
+
+void inc_depth(){
+    dplldepth++;
+}
+void dec_depth(){
+    dplldepth--;
+}
+
 bool dpll() {
-    bool active = false;
-    bool nullrule = false;
+    dpllcall++;
+    //if(dpllcall%500==0) cout << "dpllcall is" << dpllcall <<"depth is"<<dplldepth<< endl;
+    bool active = false, nullrule = false;
     while (true) {
         if (active_rules.empty()) {
             active = true;
@@ -267,8 +339,8 @@ bool dpll() {
             continue;
         break;
     }
-    vector<int> deactivated_rules_local1 = deactivated_rules;
-    vector<pair<int, int>> removed_variables_local1 = removed_variables;
+    vector<int> deactivated_rules_local1 = move(deactivated_rules);
+    vector<pair<int, int>> removed_variables_local1 = move(removed_variables);
     deactivated_rules.clear();
     removed_variables.clear();
     if (active || nullrule) {
@@ -276,28 +348,43 @@ bool dpll() {
         if (active) return true;
         if (nullrule) return false;
     }
+    inc_depth();
     int index = choose_index();
-    cnf(index, true);
-    vector<int> deactivated_rules_local2 = deactivated_rules;
-    vector<pair<int, int>> removed_variables_local2 = removed_variables;
-    deactivated_rules.clear();
-    removed_variables.clear();
-    if (dpll()) {
-        revert(deactivated_rules_local2, removed_variables_local2);
-        revert(deactivated_rules_local1, removed_variables_local1);
-        return true;
+    if(dplldepth<5){
+        cerr<<dplldepth<<" -> ";
+        print_human(index); cerr << endl;
     }
-    revert(deactivated_rules_local2, removed_variables_local2);
-    cnf(index, false);
-    deactivated_rules_local2 = deactivated_rules;
-    removed_variables_local2 = removed_variables;
+    cnf(index, true);
+    vector<int> deactivated_rules_local2 = move(deactivated_rules);
+    vector<pair<int, int>> removed_variables_local2 = move(removed_variables);
     deactivated_rules.clear();
     removed_variables.clear();
-    bool res = dpll();
+    if (dpll())
+        return true;
     revert(deactivated_rules_local2, removed_variables_local2);
+    if(!goahead()){
+        revert(deactivated_rules_local1, removed_variables_local1);
+        dec_depth();
+        return false;
+    }
+    cnf(index, false);
+    deactivated_rules_local2 = move(deactivated_rules);
+    removed_variables_local2 = move(removed_variables);
+    deactivated_rules.clear();
+    removed_variables.clear();
+    if(dplldepth<5){
+        cerr<<dplldepth<<" -> not";
+        print_human(index); cerr << endl;
+    }
+    if(dpll())
+        return true;
+    revert(deactivated_rules_local2, removed_variables_local2);
+    return_depth =  min(calc_return_depth(index),return_depth);
     revert(deactivated_rules_local1, removed_variables_local1);
-    return res;
+    dec_depth();
+    return false;
 }
+
 
 
 int main() {
@@ -381,7 +468,16 @@ int main() {
                                 }
                     }
     }
+    var_rule.max_load_factor(0.01);
+    rule_var.max_load_factor(0.01);
+    active_rules.max_load_factor(0.01);
+    pure_candidates.max_load_factor(0.01);
+    unit_rules.max_load_factor(0.01);
+    pure_candidates.max_load_factor(0.01);
+    decl.max_load_factor(0.01);
     //cout << rules.size() << endl;
+    grule_var = rule_var;
+    gvar_rule = var_rule;
     //---solving the piece
     clock_t begin = clock();
     bool solved = dpll();
