7a97f261900b32a078a77c6568e22334b2730509
miladink <mi.aghajohari@gmail.com>
2018-07-10 02:08:44 +0430

    new tests added

--- csp.h
+++ csp.h
@@ -17,3 +17,27 @@ struct Piece
     string pattern;
     vector<Place> places;
 };
+
+struct Collision
+{
+    int p1;
+    int p2;
+    Place place1;
+    Place place2;
+    size_t hash;
+
+    bool operator==(const Collision &b) const
+    {
+        return ((p1 == b.p1) && (p2 == b.p2) && (p1 == b.p1) && (place1.x == b.place1.x) &&
+                (place2.x == b.place2.x) && (place1.y == b.place1.y) && (place2.y == b.place2.y) &&
+                (place1.dir == b.place1.dir) && (place2.dir == b.place2.dir));
+    };
+};
+
+struct Collision_Hasher
+{
+    size_t operator()(const Collision &b) const
+    {
+        return b.hash;
+    };
+};
